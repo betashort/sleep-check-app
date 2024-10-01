47028b7dfe4d604ce2a1f0fe69c2da1f30e74a40
betashort <wshota.betashort@gmail.com>
2024-10-01 21:44:11 +0900

    AIS

--- sleep-check-app/src/assets/data/aisSelfCheckQA.ts
+++ sleep-check-app/src/assets/data/aisSelfCheckQA.ts
@@ -1,8 +1,51 @@
-import {IAISSelfCheckFormAnswer, IAISSelfCheckFormQuestion } from "../../shared/interface/aisSelfCheckForm";
+export interface IQuestion {
+  [key: string]: string;
+}
 
-export const aisSelfCheckQuestions: IAISSelfCheckFormQuestion[]= [
+export interface IAnswerLabel {
+  label: string;
+  score: number;
+}
 
-]
-export const aisSelfCheckQuestion1: IAISSelfCheckFormAnswer[] = [
+export interface IAnswerChoice {
+  [key: string]: {
+    [key: string]: IAnswerLabel;
+  };
+}
 
-]
+export const QUESTIONS: IQuestion = {
+  question1: "ベッドについてから実際に寝るまで、時間がかかりましたか？",
+  question2: "時間がかかりましたか？",
+};
+
+export const CHOICELIST: string[] = [
+  "choice1",
+  "choice2",
+  "choice3",
+  "choice4",
+];
+
+export const ANSWERCHOICES: IAnswerChoice[] = [
+  {
+    question1: {
+      choice1: { label: "いつもより寝つきがよかった", score: 0 },
+      choice2: { label: "いつもより少し時間がかかった", score: 1 },
+      choice3: { label: "いつもよりかなり時間がかかった", score: 2 },
+      choice4: {
+        label: "いつもより非常に時間がかかった、あるいは全く眠れなかった",
+        score: 3,
+      },
+    },
+  },
+  {
+    question2: {
+      choice1: { label: "いつもより寝つきがよかった", score: 0 },
+      choice2: { label: "いつもより少し時間がかかった", score: 1 },
+      choice3: { label: "いつもよりかなり時間がかかった", score: 2 },
+      choice4: {
+        label: "いつもより非常に時間がかかった、あるいは全く眠れなかった",
+        score: 3,
+      },
+    },
+  },
+];
--- sleep-check-app/src/pages/AISSelfCheckPage.tsx
+++ sleep-check-app/src/pages/AISSelfCheckPage.tsx
@@ -1,16 +1,37 @@
-import React from "react";
-import { IAISSelfCheckFormAnswer } from "../shared/interface/aisSelfCheckForm";
-import { SubmitHandler, useForm } from "react-hook-form";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  ANSWERCHOICES,
+  CHOICELIST,
+  IAnswerChoice,
+  QUESTIONS,
+} from "../assets/data/aisSelfCheckQA";
+
+type IQA = {
+  [question: string]: number;
+};
 
 function AISSelfCheckPage() {
+  // useState
+  const [score, setScore] = useState(0);
   // useForm
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<IAISSelfCheckFormAnswer>();
-  const onSubmit: SubmitHandler<IAISSelfCheckFormAnswer> = (data) => console.log(data);
+  } = useForm<IQA>();
+
+  let tmpScore = 0;
+  function CheckAISScore() {
+    const scoreList = Object.values(watch());
+    for (let i = 0; i < scoreList.length; i++) {
+      tmpScore += Number(scoreList[i]);
+    }
+    setScore(tmpScore);
+    console.log(score);
+  }
+
   return (
     <>
       <h2 className="p-4 text-xl text-center">
@@ -19,83 +40,47 @@ function AISSelfCheckPage() {
       <div className="p-4 flex justify-center items-center">
         <table className="">
           <tr>
-            <th className=" border border-slate-900">質問</th>
-            <th className="border border-slate-900">回答</th>
+            <th className="border bg-slate-400">質問</th>
+            <th className="border bg-slate-400">回答</th>
           </tr>
-          <tr>
-            <td className="border border-slate-900">
-              ベッドについてから実際に寝るまで、時間がかかりましたか？
-            </td>
-            <td className="border border-slate-900">
-              <form>
-                <ul>
-                  <li>
-                    <label>
-                      <input type="radio" value={0} {...(register("score"), {required:true, name:"Q1"})} />
-                      いつもより寝つきがよかった
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      <input type="radio" value={1} {...(register("score"), {required:true, name:"Q1"})} />
-                      いつもより少し時間がかかった
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      <input type="radio" value={2} {...(register("score"), {required:true, name:"Q1"})} />
-                      いつもよりかなり時間がかかった
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      <input type="radio" value={3} {...(register("score"), {required:true, name:"Q1"})} />
-                      いつもより非常に時間がかかった、あるいは全く眠れなかった
-                    </label>
-                  </li>
-                </ul>
-              </form>
-            </td>
-          </tr>
-          <tr>
-            <td className="border border-slate-900">
-              夜間、睡眠の途中で目が覚めましたか？
-            </td>
-            <td className="border border-slate-900">
-              <form>
-                <ul>
-                  <li>
-                    <label>
-                      <input type="radio" name="Q1" />
-                      問題になるほどのことはなかった
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      <input type="radio" name="Q1" />
-                      少し困ることがある
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      <input type="radio" name="Q1" />
-                      かなり困っている
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      <input type="radio" name="Q1" />
-                      深刻な状態、あるいは全く眠れなかった
-                    </label>
-                  </li>
-                </ul>
-              </form>
-            </td>
-          </tr>
+          {ANSWERCHOICES.map((answerChoice) => {
+            const keyQuestion: string = Object.keys(answerChoice)[0];
+            return (
+              <>
+                <tr>
+                  <td className="border">{QUESTIONS[keyQuestion]}</td>
+                  <td className="border">
+                    <form>
+                      <ul>
+                        {CHOICELIST.map((choice) => {
+                          return (
+                            <li>
+                              <label key={choice}>
+                                <input
+                                  type="radio"
+                                  value={
+                                    answerChoice[keyQuestion][choice].score
+                                  }
+                                  {...register(keyQuestion, { required: "" })}
+                                />
+                                {answerChoice[keyQuestion][choice].label}
+                              </label>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </form>
+                  </td>
+                </tr>
+              </>
+            );
+          })}
         </table>
-
-        <button>評価</button>
       </div>
+      <div className="flex justify-center items-center">
+        <button className=" text-xl text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"  onClick={CheckAISScore}>評価</button>
+      </div>
+      <pre>{score}</pre>
     </>
   );
 }
